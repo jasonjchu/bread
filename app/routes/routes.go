29b12f7fad266eb9af25ae30aaad8adef76442c8
Charles Zhang <gzcharleszhang@hotmail.com>
2020-02-04 09:54:17 -0500

    initial

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -2,15 +2,23 @@ package routes
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/jasonjchu/bread/app/handlers/getEmployersHandler"
 	"github.com/jasonjchu/bread/app/handlers/getJobsHandler"
 )
 
 func InitRoutes(r chi.Router) {
 	initJobRoutes(r)
+	initEmployerRoutes(r)
 }
 
 func initJobRoutes(r chi.Router) {
 	r.Route("/jobs", func(r chi.Router) {
 		r.Get(getJobsHandler.RouteURL, getJobsHandler.Handler)
+	})
+}
+
+func initEmployerRoutes(r chi.Router) {
+	r.Route("/employers", func(r chi.Router) {
+		r.Get(getEmployersHandler.RouteURL, getEmployersHandler.Handler)
 	})
 }
