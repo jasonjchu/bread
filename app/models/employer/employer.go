c82ac1d8346159d0671d6e0775f421772c5bd9f4
Kallen Tu <kallentu@gmail.com>
2020-02-09 23:55:51 -0500

    Modelled Employer + getEmployerById query. (#15)

---
+++ app/models/employer/employer.go
@@ -0,0 +1,37 @@
+package employer
+
+import (
+	"github.com/jasonjchu/bread/app/db"
+	"github.com/jmoiron/sqlx"
+)
+
+type Id int
+type Name string
+type WorksAt int
+
+type Employer struct {
+	Id      Id      `db:"_id"`
+	Name    Name    `db:"name"`
+	WorksAt WorksAt `db:"works_at"`
+}
+
+// TODO(kallentu): Write Employer handler that requests this query.
+func GetEmployerById(id Id) (*Employer, error) {
+	pool := db.Pool
+	row := pool.QueryRowx("SELECT * FROM employers WHERE _id=?", id)
+
+	employer, err := scanEmployerFromRow(row)
+	if err != nil {
+		return nil, err
+	}
+	return employer, nil
+}
+
+func scanEmployerFromRow(row *sqlx.Row) (*Employer, error) {
+	employer := Employer{}
+	err := row.StructScan(&employer)
+	if err != nil {
+		return nil, err
+	}
+	return &employer, err
+}
