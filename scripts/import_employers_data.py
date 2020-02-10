3832c05504f51f5b4da494c9ca1b6a6b96c276e1
Kallen Tu <kallentu@gmail.com>
2020-02-09 23:39:21 -0500

    Added employer test data + script to add into employers table. (#14)

---
+++ scripts/import_employers_data.py
@@ -0,0 +1,32 @@
+from db import get_db_connection, populate_table
+import os
+
+def create_table():
+    query = """
+    CREATE TABLE IF NOT EXISTS employers (
+    _id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(225),
+    works_at INT,
+    FOREIGN KEY (works_at) REFERENCES companies (_id) ON DELETE CASCADE
+    )
+    """
+    db_cxn = get_db_connection(True)
+    db_cxn.cursor().execute(query)
+
+
+def drop_table():
+    db_cxn = get_db_connection(True)
+    db_cxn.cursor().execute("DROP TABLE IF EXISTS employers")
+
+
+def populate_employers_data():
+    drop_table()
+    create_table()
+    # Only populates employer test data in testing environment.
+    if os.getenv("BREAD_ENV") == "testing":
+        data_src = 'data/employers-test.csv'
+        populate_table('employers', data_src)
+
+
+if __name__ == '__main__':
+    populate_employers_data()
