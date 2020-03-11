a5a1fe0cce36d852a92c412916f70f62657c4178
Aaron <aaronchoo9@gmail.com>
2020-03-11 03:27:12 -0400

    properly retrieved from prod

--- sql/candidate_seen_job-production.sql
+++ sql/candidate_seen_job-production.sql
@@ -1,3 +1,4 @@
 SELECT * FROM candidates WHERE _id in
-      (SELECT cid FROM candidateSeenJob WHERE jid='1e2637cb5f7a2c4615a99a26c0566c66' AND liked=True
-      AND cid NOT IN (SELECT cid FROM jobSeenCandidate WHERE jid='1e2637cb5f7a2c4615a99a26c0566c66')) LIMIT 200;
+      (SELECT cid FROM candidateSeenJob WHERE jid='002139162b354fa7b2bd9e91e408fa30' AND liked=True
+      AND cid NOT IN (SELECT cid FROM jobSeenCandidate WHERE jid='002139162b354fa7b2bd9e91e408fa30')) LIMIT 200;
+
