7cf85e448b2e1028bc44efa6afb2bf082dc9f405
Aaron <aaronchoo9@gmail.com>
2020-04-04 16:27:50 -0400

    option to filter jobs by tags and moved url param to header

--- app/handlers/getJobsForCandidatesHandler/getJobsForCandidatesHandler.go
+++ app/handlers/getJobsForCandidatesHandler/getJobsForCandidatesHandler.go
@@ -1,19 +1,38 @@
 package getJobsForCandidatesHandler
 
 import (
-	"github.com/go-chi/chi"
+	"github.com/jasonjchu/bread/app/models/candidate"
 	"github.com/jasonjchu/bread/app/models/job"
 	"github.com/jasonjchu/bread/app/utils"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
-// TODO: Change from passing the route in the param url to in the request header
-const RouteURL string = "/candidate/{id}"
-const jobLimit int = 200
+const RouteURL string = "/candidate"
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	candidateId := chi.URLParam(r, "id")
-	jobs, err := job.GetJobsByCidNotSeen(candidateId, jobLimit)
+	cid := r.Header.Get("user_id")
+	candidateId, err := strconv.Atoi(cid)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+	}
+
+	tid := r.Header.Get("tag_ids")
+	var tags []string
+	// need to remove the brackets given an array
+	if len(tid) > 2 {
+		tags = strings.Split(tid[1:len(tid)-1], ",")
+	}
+
+	// retrieve the limit if present ow default to 200
+	limit := r.Header.Get("job_limit")
+	jobLimit, err := strconv.Atoi(limit)
+	if err != nil {
+		jobLimit = 200
+	}
+
+	jobs, err := job.GetJobsByCidNotSeen(candidate.Id(candidateId), jobLimit, tags)
 	if err != nil {
 		http.Error(w, err.Error(), 400)
 		return
