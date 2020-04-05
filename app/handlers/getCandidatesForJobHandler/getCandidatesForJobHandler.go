bcd6e4a4b19b4615522a2c5b3cd414e2741db6dc
Aaron Choo <aaronchoo9@gmail.com>
2020-04-05 19:50:25 -0400

    Adding limit as part of the UR Query (#46)
    * updated candidates-for-job endpoint
    
    * moved id in url to header along with limit
    
    * added limit as an url query

--- app/handlers/getCandidatesForJobHandler/getCandidatesForJobHandler.go
+++ app/handlers/getCandidatesForJobHandler/getCandidatesForJobHandler.go
@@ -5,13 +5,20 @@ import (
 	"github.com/jasonjchu/bread/app/models/candidate"
 	"github.com/jasonjchu/bread/app/utils"
 	"net/http"
+	"strconv"
 )
 
 const RouteURL string = "/candidates"
-const candidateLimit int = 200
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	jobId := chi.URLParam(r, "job_id")
+
+	limit := r.URL.Query().Get("limit")
+	candidateLimit, err := strconv.Atoi(limit)
+	if err != nil {
+		candidateLimit = 200
+	}
+
 	candidates, err := candidate.GetCandidatesByJidLiked(jobId, candidateLimit)
 	if err != nil {
 		http.Error(w, err.Error(), 400)
