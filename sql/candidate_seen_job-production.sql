29f306994485458e2de8ca916c6030f48002bfdb
jack <zhangjack27@gmail.com>
2020-04-06 01:08:43 -0400

    added and fixed sql and out

--- sql/candidate_seen_job-production.sql
+++ sql/candidate_seen_job-production.sql
@@ -1,1 +1,7 @@
-SELECT * FROM jobs WHERE _id NOT IN (SELECT jid FROM candidateSeenJob WHERE cid = 1) LIMIT 200;
+SELECT jobs.*, companies.name as company_name
+FROM jobs,
+     companies
+WHERE jobs._id NOT IN
+      (SELECT jid FROM candidateSeenJob WHERE cid = 1)
+  AND jobs.company_id = companies._id
+LIMIT 200;
